4c7aa89c639e4d2382182ffd2f9a3d08a1f1b202
박준기 <qmqqqqm@gmail.com>
2023-06-12 00:30:36 +0900

    프론트 작업을 위해 react typescript 설치

--- was/src/main/java/com/example/demo/repository/UserRepository.java
+++ was/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {;
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+	;
 }
