b23e355602c99f119bd3cbceb159d032734b7b0c
jungi.park <qmqqqqm@gmail.com>
2023-06-14 17:36:26 +0900

    회용가입 테스트용 페이지 생성

--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -1,6 +1,16 @@
+import './Main.css';
 import Counter from '../components/Counter';
 const Main = () => {
-  return <Counter />;
+  return (
+    <div id="form">
+      <input placeholder="이름" type="text"></input>
+      <input placeholder="생일" type="text"></input>
+      <input placeholder="이메일" type="text"></input>
+      <input placeholder="비밀번호" type="text"></input>
+      <input placeholder="핸드폰번호" type="text"></input>
+      <input placeholder="성별" type="text"></input>
+    </div>
+  );
 };
 
 export default Main;
