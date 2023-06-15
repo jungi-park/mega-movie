561c638ada21c115ebd7161dc5d7d129bf143c20
박준기 <qmqqqqm@gmail.com>
2023-06-15 22:41:12 +0900

    cors 설정 완료

--- was/src/main/java/com/example/demo/WasApplication.java
+++ was/src/main/java/com/example/demo/WasApplication.java
@@ -3,17 +3,26 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 public class WasApplication {
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(WasApplication.class, args);
 	}
-	
 
- 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 
 }
