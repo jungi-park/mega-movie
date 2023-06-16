416e1edb51bb9b41fd7ba9311d8733097844d2b0
박준기 <qmqqqqm@gmail.com>
2023-06-16 21:57:35 +0900

    회원가입 정보창 인풋 이벤트 바인딩

--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -5,7 +5,7 @@ import { useDispatch } from 'react-redux';
 ;
 
 const url ='http://localhost:8080'
-
+// let userInfo ={}
 
 
 
@@ -29,21 +29,57 @@ const singUp =async() =>{
     
     const Main = () => {
 
-
     let email= ""
     const onEmailHandler = (event:any) =>{
       email = event.currentTarget.value
     }
 
-  
+    let name= ""
+    const onNameHandler = (event:any) =>{
+      name = event.currentTarget.value
+    }
+
+  let birth =""
+  const onBirthHandler = (event:any) =>{
+    birth = event.currentTarget.value
+  }
+
+  let password =""
+  const onPasswordHandler = (event:any) =>{
+    password = event.currentTarget.value
+  }
+
+  let phone =""
+  const onPhoneHandler = (event:any) =>{
+    phone = event.currentTarget.value
+  }
+
+  let sex =""
+  const onSexHandler = (event:any) =>{
+    sex = event.currentTarget.value
+  }
+
+  // userInfo ={
+  // name:name,
+  // password:password,
+  // email:email,
+  // birthDate:birth,
+  // phoneNumber:phone,
+  // sex:sex
+  // }
+
   return (
     <div id="form">
-      <input placeholder="이름" type="text"></input>
-      <input placeholder="생일" type="text"></input>
+      <input placeholder="이름" type="text" value={name} onChange={onNameHandler}></input>
+      <input placeholder="생일" type="text"  value={birth} onChange={onBirthHandler}></input>
       <input placeholder="이메일" type="text" value={email} onChange={onEmailHandler}></input>
-      <input placeholder="비밀번호" type="text"></input>
-      <input placeholder="핸드폰번호" type="text"></input>
-      <input placeholder="성별" type="text"></input>
+      <input placeholder="비밀번호" type="text"  value={password} onChange={onPasswordHandler}></input>
+      <input placeholder="핸드폰번호" type="text" value={phone} onChange={onPhoneHandler}></input>
+      <select value={sex} onChange={onSexHandler}>
+        <option value="">성별선택</option>
+        <option value="1">남자</option>
+        <option value="2">여자</option>
+      </select>
       <button onClick={singUp}>회원가입</button>
     </div>
   );
