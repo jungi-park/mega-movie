f7c56292ca010b8722189e1a537d731e7e825f83
박준기 <qmqqqqm@gmail.com>
2023-06-16 22:30:45 +0900

    회원가입 이벤트 바인딩 리팩토링

--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -1,60 +1,27 @@
 import './Main.css';
 import axios from 'axios'
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-;
+
 
 const url ='http://localhost:8080'
 
     
     const Main = () => {
 
-    const [name, setName] = React.useState("");
-    const [password, setPassword] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [phone, setPhone] = React.useState("");
-    const [birth, setBirth] = React.useState("");
-    const [sex, setSex] = React.useState("");  
+    const [form, setForm] = useState({
+      name:"",
+      password:"",
+      email:"",
+      birthDate:"",
+      phoneNumber:"",
+      sex:""
+    })
   
-    const onEmailHandler = (event:any) =>{
-      setEmail(event.currentTarget.value)
-    }
-
-  
-    const onNameHandler = (event:any) =>{
-      setName(event.currentTarget.value)
-    }
-
- 
-  const onBirthHandler = (event:any) =>{
-    setBirth(event.currentTarget.value)
-  }
-
-  
-  const onPasswordHandler = (event:any) =>{
-    setPassword(event.currentTarget.value)
-  }
-
-  
-  const onPhoneHandler = (event:any) =>{
-    setPhone(event.currentTarget.value)
-  }
-
-  
-  const onSexHandler = (event:any) =>{
-    setSex(event.currentTarget.value)
-  }
 
   const singUp =async() =>{
-    const userData = {
-      name:name,
-      password:password,
-      email:email,
-      birthDate:birth,
-      phoneNumber:phone,
-      sex:sex
-    }
-    const response = await axios.post(`${url}/v1/user`,{...userData}
+   
+
+    const response = await axios.post(`${url}/v1/user`,{...form}
         ).then((Response)=>{console.log(Response.data)})
         .catch((Error)=>{console.log(Error)});
       }
@@ -62,12 +29,12 @@ const url ='http://localhost:8080'
 
   return (
     <div id="form">
-      <input placeholder="이름" type="text" value={name} onChange={onNameHandler}></input>
-      <input placeholder="생일" type="text"  value={birth} onChange={onBirthHandler}></input>
-      <input placeholder="이메일" type="text" value={email} onChange={onEmailHandler}></input>
-      <input placeholder="비밀번호" type="text"  value={password} onChange={onPasswordHandler}></input>
-      <input placeholder="핸드폰번호" type="text" value={phone} onChange={onPhoneHandler}></input>
-      <select value={sex} onChange={onSexHandler}>
+      <input placeholder="이름" type="text" value={form.name} onChange={event=>setForm({...form,name:event.currentTarget.value})}></input>
+      <input placeholder="생일" type="text"  value={form.birthDate} onChange={event=>setForm({...form,birthDate:event.currentTarget.value})}></input>
+      <input placeholder="이메일" type="text" value={form.email} onChange={event=>setForm({...form,email:event.currentTarget.value})}></input>
+      <input placeholder="비밀번호" type="text"  value={form.password} onChange={event=>setForm({...form,password:event.currentTarget.value})}></input>
+      <input placeholder="핸드폰번호" type="text" value={form.phoneNumber} onChange={event=>setForm({...form,phoneNumber:event.currentTarget.value})}></input>
+      <select value={form.sex} onChange={event=>setForm({...form,sex:event.currentTarget.value})}>
         <option value="">성별선택</option>
         <option value="1">남자</option>
         <option value="2">여자</option>
