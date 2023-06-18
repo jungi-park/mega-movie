1871c25d63eae1f110f894cd6cca39bf7520fb37
박준기 <qmqqqqm@gmail.com>
2023-06-18 23:44:26 +0900

    jwt 생성시 생성시간, 만료시간 추가

--- was/src/main/java/com/example/demo/service/JwtService.java
+++ was/src/main/java/com/example/demo/service/JwtService.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public interface JwtService {
 	 
-	 public String createToken(String id);
+	 public String createToken(String email);
 	 
 	 public Map<String, Object> verifyJWT(String jwt) throws UnsupportedEncodingException;
 }
