8cc136301bd7c42f581f9b9bea9bffd724b01aa9
박준기 <qmqqqqm@gmail.com>
2023-06-21 22:10:59 +0900

    메인페이지 라우팅 설정 완료

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -6,6 +6,7 @@ import rootReducer from './modules/rootReducer';
 import { createStore } from 'redux';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Main from './pages/Main';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,8 +17,9 @@ root.render(
   <BrowserRouter>
     <Provider store={store}>
       <Routes>
-        <Route path='/signup' element={<SignUp></SignUp>}></Route>
-        <Route path='/signin' element={<SignIn></SignIn>}></Route>
+      <Route path='/*' element={<Main></Main>}></Route>
+      <Route path='/signup' element={<SignUp></SignUp>}></Route>
+      <Route path='/signin' element={<SignIn></SignIn>}></Route>
       </Routes>
     </Provider>
   </BrowserRouter>
