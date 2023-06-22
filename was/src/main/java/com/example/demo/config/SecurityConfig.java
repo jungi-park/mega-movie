5feb7ea79258c8f01682180064792468e3c53e11
박준기 <qmqqqqm@gmail.com>
2023-06-23 01:02:54 +0900

    스프링 시큐리티 의존성 추가후 cors 오류 수정중

--- was/src/main/java/com/example/demo/config/SecurityConfig.java
+++ was/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.example.demo.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @EnableWebSecurity
@@ -14,19 +19,35 @@ public class SecurityConfig {
 	@Autowired
 	private TokenProvider tokenProvider;
 
+
 	@Bean
 	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	        http.csrf().disable()
-	        .httpBasic().disable();
+	        .httpBasic().disable()
+	        .cors(cors -> cors.disable());
 	        http.authorizeHttpRequests()
 	                .requestMatchers("/v1/user/**").authenticated()
 	                .requestMatchers("/v1/admin/**").hasAnyRole("hasRole('ROLE_ADMIN')")
 	                .anyRequest().permitAll();
+	        
 	        
 	        http.addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
 	          
 
 	        return http.build();
 	    }
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("<http://localhost:3000>", "..."));
+	    configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+	    configuration.setAllowCredentials(true);
+	    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Authorization-refresh", "Cache-Control", "Content-Type"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
+	}
+
 
 }
---
+++ was/src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/v1")
+public class AdminController {
+
+	@GetMapping("admin")
+	public String testAdmin() {
+		return "성공";
+	}
+}
