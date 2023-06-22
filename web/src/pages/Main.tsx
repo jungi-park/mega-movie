cb1402a8983980e4bfd30d5e4e71496270db3fa5
박준기 <qmqqqqm@gmail.com>
2023-06-23 01:02:19 +0900

    스프링 시큐리티 어드민 권한 설정 확인 테스트 로직 작성

--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -1,8 +1,20 @@
+import axios from "axios";
 import React from "react";
 
 
 function Main(){
-    return null
+    const url ='http://localhost:8080'
+    const sendTestAdmin = async() =>{
+        const response = await axios.get(`${url}/v1/admin`,{ withCredentials: true }
+        ).then((Response)=>{
+            console.log("Response",Response)
+        })
+        .catch((Error)=>{console.log(Error)});
+      }
+
+      return(<>
+      <button onClick={sendTestAdmin}>testAdmin</button>
+      </>)
 }
 
 export default Main
