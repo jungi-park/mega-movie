78590d632be87dfd4f81a1aea4a0e4c33d0b9345
박준기 <qmqqqqm@gmail.com>
2023-06-25 19:42:21 +0900

    session 사용중지 및 jwt필터 주석해제

--- was/src/main/java/com/example/demo/config/SecurityConfig.java
+++ was/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -23,6 +25,9 @@ public class SecurityConfig {
 	        http.csrf().disable()
 	        .httpBasic().disable()
 	        .cors().configurationSource(corsConfigurationSource());
+	        
+	        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	        
 	        http.authorizeHttpRequests()
 //	        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
 	                .requestMatchers("/v1/user/**").authenticated()
@@ -30,7 +35,7 @@ public class SecurityConfig {
 	                .requestMatchers("/v1/**").permitAll();
 	        
 	        
-//	        http.addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+	        http.addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
 	          
 
 	        return http.build();
