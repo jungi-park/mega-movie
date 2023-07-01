c9957ca22dc78737f8567079a51a1e8538a75b1f
박준기 <qmqqqqm@gmail.com>
2023-07-01 20:50:46 +0900

    로그인 후 메인페이지 링크 설정

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './SignIn.scss';
 import axios from 'axios';
 import { loginUser, logoutUser } from '../modules/user';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../modules/rootReducer';
+import { Link } from 'react-router-dom';
 
 const SingIn =() =>{
     const user = useSelector((state: RootState) => state.userReducer);
@@ -15,6 +16,8 @@ const SingIn =() =>{
         password:"",
         email:"",
       })
+
+      
 
     const sendSingIn = async() =>{
         const response = await axios.post(`${url}/v1/login`,{...form},{ withCredentials: true }
@@ -42,6 +45,7 @@ const SingIn =() =>{
               <input type='password' placeholder='비밀번호'  value={form.password} onChange={event=>setForm({...form,password:event.target.value})}></input>
               {!user.isLogin&&<button onClick={sendSingIn}>로그인</button>}
               {user.isLogin&&<button onClick={sendSingOut}>로그아웃</button>}
+              {user.isLogin&& < Link to={"/"}>메인페이지로 가기</Link>}
           </div>
       )
     
