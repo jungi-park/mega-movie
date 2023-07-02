40097b335572161c50cc101a2c6b4772a99410d4
박준기 <qmqqqqm@gmail.com>
2023-07-02 18:48:55 +0900

    로그인시에 메인페이지로 이동

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -4,10 +4,12 @@ import axios from 'axios';
 import { loginUser, logoutUser } from '../modules/user';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../modules/rootReducer';
-import { Link } from 'react-router-dom';
+import { Link ,useNavigate} from 'react-router-dom';
+
 
 const SingIn =() =>{
     const user = useSelector((state: RootState) => state.userReducer);
+    let navigate = useNavigate();
     const url ='http://localhost:8080'
     const dispatch = useDispatch(); // 디스패치 함수를 가져옵니다
 
@@ -23,6 +25,7 @@ const SingIn =() =>{
         const response = await axios.post(`${url}/v1/login`,{...form},{ withCredentials: true }
         ).then((Response)=>{if(Response.data.email){
             dispatch(loginUser({email:Response.data.email,name:Response.data.name}))
+            navigate("/");
             console.log("로그인",user)
         }})
         .catch((Error)=>{console.log(Error)});
@@ -45,7 +48,6 @@ const SingIn =() =>{
               <input type='password' placeholder='비밀번호'  value={form.password} onChange={event=>setForm({...form,password:event.target.value})}></input>
               {!user.isLogin&&<button onClick={sendSingIn}>로그인</button>}
               {user.isLogin&&<button onClick={sendSingOut}>로그아웃</button>}
-              {user.isLogin&& < Link to={"/"}>메인페이지로 가기</Link>}
           </div>
       )
     
