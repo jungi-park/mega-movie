c5e7571110b710a41d9ce34eb31e7b029fdd58de
박준기 <qmqqqqm@gmail.com>
2023-07-18 22:06:12 +0900

    Genre 컬럼 enum 타입으로 변경

--- was/src/main/java/com/example/demo/entity/MovieEntity.java
+++ was/src/main/java/com/example/demo/entity/MovieEntity.java
@@ -4,9 +4,13 @@ import java.util.Date;
 
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.type.EnumType;
+
+import com.example.demo.type.Genre;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,6 +34,18 @@ public class MovieEntity {
 	@Column(updatable = false, columnDefinition = "TIMESTAMP")
 	private Date releasDate;
 	
+	@Enumerated(jakarta.persistence.EnumType.STRING)
 	@Column(nullable = false, length = 100)
-	private String genre;
+	private Genre genre;
+	
+	
+
+	public MovieEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+	
 }
