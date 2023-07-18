a520a695a331cc216338d8014f03df79f9538320
박준기 <qmqqqqm@gmail.com>
2023-07-18 22:07:58 +0900

    MovieEntity getter,setter,tosting 생성

--- was/src/main/java/com/example/demo/entity/MovieEntity.java
+++ was/src/main/java/com/example/demo/entity/MovieEntity.java
@@ -21,7 +21,7 @@ import jakarta.persistence.TemporalType;
 @DynamicInsert
 @Entity(name = "movie")
 public class MovieEntity {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(updatable = false, nullable = false, columnDefinition = "INT UNSIGNED")
@@ -29,23 +29,64 @@ public class MovieEntity {
 
 	@Column(nullable = false, length = 100)
 	private String title;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(updatable = false, columnDefinition = "TIMESTAMP")
 	private Date releasDate;
-	
+
 	@Enumerated(jakarta.persistence.EnumType.STRING)
 	@Column(nullable = false, length = 100)
 	private Genre genre;
-	
-	
 
 	public MovieEntity() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
+	public MovieEntity(int movieId, String title, Date releasDate, Genre genre) {
+		super();
+		this.movieId = movieId;
+		this.title = title;
+		this.releasDate = releasDate;
+		this.genre = genre;
+	}
 
-	
-	
+	public int getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getReleasDate() {
+		return releasDate;
+	}
+
+	public void setReleasDate(Date releasDate) {
+		this.releasDate = releasDate;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	@Override
+	public String toString() {
+		return "MovieEntity [movieId=" + movieId + ", title=" + title + ", releasDate=" + releasDate + ", genre="
+				+ genre + "]";
+	}
+
 }
