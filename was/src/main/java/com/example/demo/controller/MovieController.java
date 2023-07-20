0dc11575ba0420e977e78cd472b3931ca6e2c168
박준기 <qmqqqqm@gmail.com>
2023-07-20 22:26:09 +0900

    movieController 생성

---
+++ was/src/main/java/com/example/demo/controller/MovieController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.MovieEntity;
+import com.example.demo.service.MovieService;
+
+@RestController
+@RequestMapping("/v1")
+public class MovieController {
+
+
+}
