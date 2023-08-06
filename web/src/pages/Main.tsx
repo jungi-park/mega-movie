19046d252f960e14d6e112c77464b41fe5f9ca3a
DESKTOP-0S7NVQT\Admin <inahk006@gmail.com>
2023-08-06 13:37:49 +0900

    [퍼블리싱] main 레이아웃

--- web/src/pages/Main.tsx
+++ web/src/pages/Main.tsx
@@ -37,25 +37,104 @@ function Main() {
     <>
       <Header></Header>
       <main className="mainContainer">
-        <section className="HeroBanner">
-          <div className="bannerBg">
-            <img src={process.env.PUBLIC_URL + "/img_main_1.png"} alt="" />
-            <div className="bannerGrad"></div>
-          </div>
-          <div className="bannerCont">
-            <div className="title">
-              <img
-                className="titleLogo"
-                src={process.env.PUBLIC_URL + "/logo_main_1.png"}
-                alt=""
-              />
-              <h1 className="titleKor">더 퍼스트 슬램덩크</h1>
-              <h2 className="titleEng">The First Slam Dunk</h2>
+        <div className="bannerSection">
+          <div className="HeroBanner">
+            <div className="bannerBg">
+              <img src={process.env.PUBLIC_URL + "/img_main_1.png"} alt="" />
+              <div className="bannerGrad"></div>
             </div>
-            <button className="ticketing">예매하기</button>
+            <div className="bannerCont">
+              <div className="title">
+                <img
+                  className="titleLogo"
+                  src={process.env.PUBLIC_URL + "/logo_main_1.png"}
+                  alt=""
+                />
+                <h1 className="titleKor">더 퍼스트 슬램덩크</h1>
+                <h2 className="titleEng">The First Slam Dunk</h2>
+              </div>
+              <button className="ticketing">예매하기</button>
+            </div>
           </div>
-        </section>
+        </div>
+
+        <div className="movieSection">
+          <div className="boxOffice">
+            <h2 className="title">
+              박스 오피스<span>08.15 18:30 기준</span>
+            </h2>
+            <div className="movieSlide">
+              <div className="movieList">
+                <div className="movie"></div>
+                <div className="movie"></div>
+                <div className="movie"></div>
+                <div className="movie"></div>
+                <div className="movie"></div>
+                <div className="movie"></div>
+                <div className="movie"></div>
+                <div className="movie"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="eventSection">
+          <div className="benefit">
+            <h2 className="title">
+              혜택<span>08.15 18:30 기준</span>
+            </h2>
+            <div className="eventContent">
+              <div className="eventInfo">
+                <div className="eventTitle"></div>
+                <div className="eventDate"></div>
+              </div>
+              <div className="eventList">
+                <div className="eventScreen"></div>
+                <div className="eventNav"></div>
+              </div>
+            </div>
+          </div>
+          <div className="eventBg"></div>
+        </div>
+
+        <div className="curationSection">
+          <div className="curation">
+            <h2 className="title">
+              큐레이션<span>08.15 18:30 기준</span>
+            </h2>
+            <div className="curationContent">
+              <div className="curationInfo">
+                <div className="curationTitle"></div>
+                <div className="curationDate"></div>
+              </div>
+              <div className="curationtSlide">
+                <div className="curationtList">
+                  <div className="curationCard"></div>
+                  <div className="curationCard"></div>
+                  <div className="curationCard"></div>
+                  <div className="curationCard"></div>
+                  <div className="curationCard"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="guideSection">
+          <div className="guide">
+            <h2 className="title">안내</h2>
+            <div className="guideList">
+              <div className="guideCard"></div>
+              <div className="guideCard"></div>
+              <div className="guideCard"></div>
+              <div className="guideCard"></div>
+              <div className="guideCard"></div>
+              <div className="guideCard"></div>
+            </div>
+          </div>
+        </div>
       </main>
+      <footer></footer>
     </>
   );
 }
