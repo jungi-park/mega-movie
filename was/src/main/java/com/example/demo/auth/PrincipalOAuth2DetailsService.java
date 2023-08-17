ad2e05dbb33872a3c8fa9531ddd4cbcbd2e67013
박준기 <qmqqqqm@gmail.com>
2023-08-17 22:26:46 +0900

    PrincipalOAuth2DetailsService 클래스 정리

--- was/src/main/java/com/example/demo/auth/PrincipalOAuth2DetailsService.java
+++ was/src/main/java/com/example/demo/auth/PrincipalOAuth2DetailsService.java
@@ -34,29 +34,13 @@ public class PrincipalOAuth2DetailsService extends DefaultOAuth2UserService {
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		OAuth2User oAuth2User = super.loadUser(userRequest);
 
-//		String registrationId = userRequest.getClientRegistration().getRegistrationId();
-//		String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint()
-//				.getUserNameAttributeName();
-//
-//		String username = oAuth2User.getAttributes().get("name").toString();
-//		String nickname = oAuth2User.getAttributes().get("name").toString();
-//		String email = oAuth2User.getAttributes().get("email").toString();
-//		String password = passwordEncoder.encode(email);
-//		String role = "ROLE_USER";
-//		String provider = userRequest.getClientRegistration().getRegistrationId();
-//		String provider_id = oAuth2User.getAttributes().get("sub").toString();
-
-		UserEntity user = setUser(userRequest,oAuth2User);
+		UserEntity user = setUser(userRequest, oAuth2User);
 
 		try {
 			if (userService.checkEmail(user.getEmail()) == 0) {
-//	                log.info("구글 로그인이 최초입니다. 회원가입을 진행합니다.");
 				userService.signUp(user);
 			} else {
-//	                log.info("구글 로그인 기록이 있습니다.");
-				// retrieveByEmail 구현 필요!!
 				user = userService.retrieveByEmail(user.getEmail()).orElseGet(null);
-
 			}
 		} catch (Exception e) {
 			throw new RuntimeException(e);
@@ -85,25 +69,25 @@ public class PrincipalOAuth2DetailsService extends DefaultOAuth2UserService {
 			Map<String, Object> response = (Map<String, Object>) oAuth2User.getAttributes().get("response");
 			String username = (String) response.get("name");
 			String email = (String) response.get("email");
-			String password =  passwordEncoder.encode(UUID.randomUUID().toString());
+			String password = passwordEncoder.encode(UUID.randomUUID().toString());
 			user.setName(username);
 			user.setEmail(email);
 			user.setPassword(password);
 			return user;
-		}else if (userRequest.getClientRegistration().getRegistrationId().equals("kakao")) {
-	        Map<String, Object> attributes = oAuth2User.getAttributes();
-	        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
-	        Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");
+		} else if (userRequest.getClientRegistration().getRegistrationId().equals("kakao")) {
+			Map<String, Object> attributes = oAuth2User.getAttributes();
+			Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
+			Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");
 
-	        String username = (String) profile.get("nickname");
-	        String email = (String) kakao_account.get("email");
-	        String password = passwordEncoder.encode(UUID.randomUUID().toString());
+			String username = (String) profile.get("nickname");
+			String email = (String) kakao_account.get("email");
+			String password = passwordEncoder.encode(UUID.randomUUID().toString());
 
-	        user.setName(username);
-	        user.setEmail(email);
-	        user.setPassword(password);
-	        return user;
-	    }
+			user.setName(username);
+			user.setEmail(email);
+			user.setPassword(password);
+			return user;
+		}
 		return null;
 	}
 }
