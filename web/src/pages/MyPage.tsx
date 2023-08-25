6a7814968664c3652950d444ee4497de77cb97f6
박준기 <qmqqqqm@gmail.com>
2023-08-25 21:36:54 +0900

    마이페이지 데이터 바인딩

--- web/src/pages/MyPage.tsx
+++ web/src/pages/MyPage.tsx
@@ -1,16 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../modules/rootReducer";
 import { getMyPage } from "../utile/DAO/user";
+import moment from "moment";
 
 export default function MyPage() {
   const user = useSelector((state: RootState) => state.userReducer);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    birthDate: "",
+    sex: "",
+    createdAt: "",
+  });
+
   useEffect(() => {
     // const userEmail = { email: user.email };
-    console.log("userEmailuserEmail", user.email);
     getMyPage(user.email).then((result) => {
-      console.log("result", result);
+      const createDate = result.data.createdAt;
+      const formattedDate = formatDate(createDate);
+      const userInfo = { ...result.data, createdAt: formattedDate };
+      setUserInfo(userInfo);
     });
-  }, []);
-  return <div>myPage</div>;
+  }, [user.email]);
+
+  const formatDate = (date: string) => {
+    return moment(date).format("YYYY-MM-DD HH:mm:ss");
+  };
+
+  return (
+    <div>
+      <div>myPage</div>
+      <div>이름: {userInfo?.name}</div>
+      <div>이메일: {userInfo?.email}</div>
+      <div>핸드폰번호: {userInfo?.phoneNumber}</div>
+      <div>생년월일: {userInfo?.birthDate}</div>
+      <div>성별: {userInfo?.sex === "1" ? "남자" : "여자"}</div>
+      <div>생성날자: {userInfo?.createdAt}</div>
+    </div>
+  );
 }
